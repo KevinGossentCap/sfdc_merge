159b8cb4238da1fe248af3ce5f13ac6c943c27be
GOSSENT <kevin.gossent@capgemini.com>
2020-03-30 18:02:34 +0200

    Check inputs for join

--- src/commands/sfdx-md-merge-driver/join.ts
+++ src/commands/sfdx-md-merge-driver/join.ts
@@ -1,4 +1,5 @@
 import {Command, flags} from '@oclif/command'
+import * as fs from 'fs'
 
 export default class Join extends Command {
   static description = 'describe the command here'
@@ -18,6 +19,18 @@ export default class Join extends Command {
     // const name = flags.name || 'world'
     // this.log(`hello ${name} from C:\\GitRepos\\sfdc_md_merge_driver\\src\\commands\\join.ts`)
 
+    if (flags.meta === undefined) {
+      console.log('list of permissions to merge is empty')
+      // eslint-disable-next-line no-throw-literal
+      throw 'list of permissions to merge is empty'
+    }
+    flags.meta.forEach(permission => {
+      if (!fs.existsSync(permission)) {
+        console.error(`${permission} is not accessible`)
+        // eslint-disable-next-line no-throw-literal
+        throw `${permission} is not accessible`
+      }
+    })
     console.log(flags.meta)
   }
 }
