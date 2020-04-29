37eae7b8fccd121ab116ca4df9978c2550baad13
GOSSENT <kevin.gossent@capgemini.com>
2020-04-29 18:20:19 +0200

    Adding to new config way
    - CustomLabels
    - CustomObjectTranslation
    - GlobalValueSetTranslation
    - StandardValueSetTranslation
    - Translations

--- src/utils/generic-meta-node.ts
+++ src/utils/generic-meta-node.ts
@@ -150,10 +150,218 @@ const PermissionSet = Object.freeze({
   },
 })
 
+const CustomLabels = Object.freeze({
+  labels: {
+    isArray: true,
+    mdtype: mdtypes.CustomLabel,
+  },
+})
+
+const CustomObjectTranslation = Object.freeze({
+  caseValues: {
+    isArray: true,
+    mdtype: mdtypes.ObjectNameCaseValue,
+  },
+  fields: {
+    isArray: true,
+    mdtype: mdtypes.CustomFieldTranslation,
+  },
+  fieldSets: {
+    isArray: true,
+    mdtype: mdtypes.FieldSetTranslation,
+  },
+  gender: {
+    mdtype: mdtypes.String,
+  },
+  layouts: {
+    isArray: true,
+    mdtype: mdtypes.LayoutTranslation,
+  },
+  nameFieldLabel: {
+    mdtype: mdtypes.String,
+  },
+  quickActions: {
+    isArray: true,
+    mdtype: mdtypes.QuickActionTranslation,
+  },
+  recordTypes: {
+    isArray: true,
+    mdtype: mdtypes.RecordTypeTranslation,
+  },
+  sharingReasons: {
+    isArray: true,
+    mdtype: mdtypes.SharingReasonTranslation,
+  },
+  startsWith: {
+    mdtype: mdtypes.String,
+  },
+  validationRules: {
+    isArray: true,
+    mdtype: mdtypes.ValidationRuleTranslation,
+  },
+  webLinks: {
+    isArray: true,
+    mdtype: mdtypes.WebLinkTranslation,
+  },
+  workflowTasks: {
+    isArray: true,
+    mdtype: mdtypes.WorkflowTaskTranslation,
+  },
+  description: {
+    mdtype: mdtypes.String,
+  },
+  help: {
+    mdtype: mdtypes.String,
+  },
+  label: {
+    mdtype: mdtypes.String,
+  },
+  lookupFilter: {
+    isArray: false,
+    mdtype: mdtypes.LookupFilterTranslation,
+  },
+  name: {
+    mdtype: mdtypes.String,
+  },
+  picklistValues: {
+    isArray: true,
+    mdtype: mdtypes.PicklistValueTranslation,
+  },
+  relationshipLabel: {
+    mdtype: mdtypes.String,
+  },
+  sections: {
+    isArray: true,
+    mdtype: mdtypes.LayoutSectionTranslation,
+  },
+})
+
+const GlobalValueSetTranslation = Object.freeze({
+  valueTranslation: {
+    isArray: true,
+    mdtype: mdtypes.ValueTranslation,
+  },
+})
+
+const StandardValueSetTranslation = Object.freeze({
+  valueTranslation: {
+    isArray: true,
+    mdtype: mdtypes.ValueTranslation,
+  },
+})
+
+const Translations = Object.freeze({
+  customApplications: {
+    isArray: true,
+    mdtype: mdtypes.CustomApplicationTranslation,
+  },
+  customLabels: {
+    isArray: true,
+    mdtype: mdtypes.CustomLabelTranslation,
+  },
+  customPageWebLinks: {
+    isArray: true,
+    mdtype: mdtypes.CustomPageWebLinkTranslation,
+  },
+  customTabs: {
+    isArray: true,
+    mdtype: mdtypes.CustomTabTranslation,
+  },
+  flowDefinitions: {
+    isArray: true,
+    mdtype: mdtypes.FlowDefinitionTranslation,
+  },
+  prompts: {
+    isArray: true,
+    mdtype: mdtypes.PromptTranslation,
+  },
+  quickActions: {
+    isArray: true,
+    mdtype: mdtypes.GlobalQuickActionTranslation,
+  },
+  reportTypes: {
+    isArray: true,
+    mdtype: mdtypes.ReportTypeTranslation,
+  },
+  scontrols: {
+    isArray: true,
+    mdtype: mdtypes.ScontrolTranslation,
+  },
+  promptVersions: {
+    isArray: true,
+    mdtype: mdtypes.PromptVersionTranslation,
+  },
+  flows: {
+    isArray: true,
+    mdtype: mdtypes.FlowTranslation,
+  },
+  choices: {
+    isArray: true,
+    mdtype: mdtypes.FlowChoiceTranslation,
+  },
+  screens: {
+    isArray: true,
+    mdtype: mdtypes.FlowScreenTranslation,
+  },
+  stages: {
+    isArray: true,
+    mdtype: mdtypes.FlowStageTranslation,
+  },
+  userInput: {
+    isArray: false,
+    mdtype: mdtypes.FlowChoiceUserInputTranslation,
+  },
+  label: {
+    mdtype: mdtypes.String,
+  },
+  choiceText: {
+    mdtype: mdtypes.String,
+  },
+  promptText: {
+    mdtype: mdtypes.String,
+  },
+  errorMessage: {
+    mdtype: mdtypes.String,
+  },
+  validationRule: {
+    isArray: false,
+    mdtype: mdtypes.FlowInputValidationRuleTranslation,
+  },
+  helpText: {
+    mdtype: mdtypes.String,
+  },
+  pausedText: {
+    mdtype: mdtypes.String,
+  },
+  fields: {
+    isArray: true,
+    mdtype: mdtypes.FlowScreenFieldTranslation,
+  },
+  fieldText: {
+    mdtype: mdtypes.String,
+  },
+  description: {
+    mdtype: mdtypes.String,
+  },
+  sections: {
+    isArray: true,
+    mdtype: mdtypes.ReportTypeSectionTranslation,
+  },
+  columns: {
+    isArray: true,
+    mdtype: mdtypes.ReportTypeColumnTranslation,
+  },
+})
+
 export const fullMdConfig = Object.freeze({
   Package: Package,
   Profile: Profile,
   PermissionSet: PermissionSet,
+  CustomLabels: CustomLabels,
+  CustomObjectTranslation: CustomObjectTranslation,
+  GlobalValueSetTranslation: GlobalValueSetTranslation,
+  StandardValueSetTranslation: StandardValueSetTranslation,
+  Translations: Translations,
 })
 
 export function getSupportedMeta() {
@@ -163,98 +371,3 @@ export function getSupportedMeta() {
 export function getMetaConfig(meta: string) {
   return fullMdConfig[meta]
 }
-
-export const configJson = Object.freeze({
-  supported: ['Package', 'Profile'],
-  mdnodes: {
-    version: {mdtype: mdtypes.String},
-    types: {
-      isArray: true,
-      mdtype: mdtypes.PackageTypeMembers,
-    },
-    name: {mdtype: mdtypes.String},
-    members: {
-      isArray: true,
-      mdtype: mdtypes.String,
-    },
-    applicationVisibilities: {
-      isArray: true,
-      mdtype: mdtypes.ProfileApplicationVisibility,
-    },
-    categoryGroupVisibilities: {
-      isArray: true,
-      mdtype: mdtypes.ProfileCategoryGroupVisibility,
-    },
-    dataCategories: {
-      isArray: true,
-      mdtype: mdtypes.String,
-    },
-    classAccesses: {
-      isArray: true,
-      mdtype: mdtypes.ProfileApexClassAccess,
-    },
-    custom: {mdtype: mdtypes.String},
-    customMetadataTypeAccesses: {
-      isArray: true,
-      mdtype: mdtypes.ProfileCustomMetadataTypeAccess,
-    },
-    customPermissions: {
-      isArray: true,
-      mdtype: mdtypes.ProfileCustomPermissions,
-    },
-    customSettingAccesses: {
-      isArray: true,
-      mdtype: mdtypes.ProfileCustomSettingAccesses,
-    },
-    description: {mdtype: mdtypes.String},
-    externalDataSourceAccesses: {
-      isArray: true,
-      mdtype: mdtypes.ProfileExternalDataSourceAccess,
-    },
-    fieldPermissions: {
-      isArray: true,
-      mdtype: mdtypes.ProfileFieldLevelSecurity,
-    },
-    flowAccesses: {
-      isArray: true,
-      mdtype: mdtypes.flowAccesses,
-    },
-    layoutAssignments: {
-      isArray: true,
-      mdtype: mdtypes.ProfileLayoutAssignments,
-    },
-    loginHours: {
-      isArray: true,
-      mdtype: mdtypes.ProfileLoginHours,
-    },
-    loginIpRanges: {
-      isArray: true,
-      mdtype: mdtypes.ProfileLoginIpRange,
-    },
-    objectPermissions: {
-      isArray: true,
-      mdtype: mdtypes.ProfileObjectPermissions,
-    },
-    pageAccesses: {
-      isArray: true,
-      mdtype: mdtypes.ProfileApexPageAccess,
-    },
-    profileActionOverrides: {
-      isArray: true,
-      mdtype: mdtypes.ProfileActionOverride,
-    },
-    recordTypeVisibilities: {
-      isArray: true,
-      mdtype: mdtypes.ProfileRecordTypeVisibility,
-    },
-    tabVisibilities: {
-      isArray: true,
-      mdtype: mdtypes.ProfileTabVisibility,
-    },
-    userLicense: {mdtype: mdtypes.String},
-    userPermissions: {
-      isArray: true,
-      mdtype: mdtypes.ProfileUserPermission,
-    },
-  },
-})
--- src/utils/generic-meta-types.ts
+++ src/utils/generic-meta-types.ts
@@ -79,6 +79,55 @@ const PermissionSet = Object.freeze({
   PermissionSetUserPermission: Profile.ProfileUserPermission,
 })
 
+const CustomLabels = Object.freeze({
+  CustomLabel: {unicity: 'full', keys: ['fullName._']},
+})
+
+const CustomObjectTranslation = Object.freeze({
+  ObjectNameCaseValue: {
+    unicity: 'full',
+    keys: ['caseType._', 'plural._', 'possessive._'],
+  },
+  CustomFieldTranslation: {unicity: 'full', keys: ['name._']},
+  FieldSetTranslation: {unicity: 'full', keys: ['name._']},
+  LayoutTranslation: {unicity: 'full', keys: ['layout._']},
+  QuickActionTranslation: {unicity: 'full', keys: ['name._']},
+  RecordTypeTranslation: {unicity: 'full', keys: ['name._']},
+  SharingReasonTranslation: {unicity: 'full', keys: ['name._']},
+  ValidationRuleTranslation: {unicity: 'full', keys: ['name._']},
+  WebLinkTranslation: {unicity: 'full', keys: ['name._']},
+  WorkflowTaskTranslation: {unicity: 'full', keys: ['name._']},
+  LookupFilterTranslation: {},
+  PicklistValueTranslation: {unicity: 'full', keys: ['masterLabel._']},
+  LayoutSectionTranslation: {unicity: 'full', keys: ['section._']},
+})
+
+const GlobalValueSetTranslation = Object.freeze({
+  ValueTranslation: {unicity: 'full', keys: ['masterLabel._']},
+})
+
+const Translations = Object.freeze({
+  CustomApplicationTranslation: {unicity: 'full', keys: ['name._']},
+  CustomLabelTranslation: {unicity: 'full', keys: ['name._']},
+  CustomPageWebLinkTranslation: {unicity: 'full', keys: ['name._']},
+  CustomTabTranslation: {unicity: 'full', keys: ['name._']},
+  FlowDefinitionTranslation: {unicity: 'full', keys: ['fullName._']},
+  PromptTranslation: {unicity: 'full', keys: ['name._']},
+  GlobalQuickActionTranslation: {unicity: 'full', keys: ['name._']},
+  ReportTypeTranslation: {unicity: 'full', keys: ['name._']},
+  ScontrolTranslation: {unicity: 'full', keys: ['name._']},
+  PromptVersionTranslation: {unicity: 'full', keys: ['name._']},
+  FlowTranslation: {unicity: 'full', keys: ['fullName._']},
+  FlowChoiceTranslation: {unicity: 'full', keys: ['name._']},
+  FlowScreenTranslation: {unicity: 'full', keys: ['name._']},
+  FlowStageTranslation: {unicity: 'full', keys: ['name._']},
+  FlowChoiceUserInputTranslation: {unicity: 'full', keys: ['name._']},
+  FlowInputValidationRuleTranslation: {},
+  FlowScreenFieldTranslation: {unicity: 'full', keys: ['name._']},
+  ReportTypeSectionTranslation: {unicity: 'full', keys: ['name._']},
+  ReportTypeColumnTranslation: {unicity: 'full', keys: ['name._']},
+})
+
 export const mdtypes = Object.freeze({
   String: String,
   PackageTypeMembers: Package.PackageTypeMembers,
@@ -117,4 +166,39 @@ export const mdtypes = Object.freeze({
     PermissionSet.PermissionSetRecordTypeVisibility,
   PermissionSetTabSetting: PermissionSet.PermissionSetTabSetting,
   PermissionSetUserPermission: PermissionSet.PermissionSetUserPermission,
+  CustomLabel: CustomLabels.CustomLabel,
+  ObjectNameCaseValue: CustomObjectTranslation.ObjectNameCaseValue,
+  CustomFieldTranslation: CustomObjectTranslation.CustomFieldTranslation,
+  FieldSetTranslation: CustomObjectTranslation.FieldSetTranslation,
+  LayoutTranslation: CustomObjectTranslation.LayoutTranslation,
+  QuickActionTranslation: CustomObjectTranslation.QuickActionTranslation,
+  RecordTypeTranslation: CustomObjectTranslation.RecordTypeTranslation,
+  SharingReasonTranslation: CustomObjectTranslation.SharingReasonTranslation,
+  ValidationRuleTranslation: CustomObjectTranslation.ValidationRuleTranslation,
+  WebLinkTranslation: CustomObjectTranslation.WebLinkTranslation,
+  WorkflowTaskTranslation: CustomObjectTranslation.WorkflowTaskTranslation,
+  LookupFilterTranslation: CustomObjectTranslation.LookupFilterTranslation,
+  PicklistValueTranslation: CustomObjectTranslation.PicklistValueTranslation,
+  LayoutSectionTranslation: CustomObjectTranslation.LayoutSectionTranslation,
+  ValueTranslation: GlobalValueSetTranslation.ValueTranslation,
+  CustomApplicationTranslation: Translations.CustomApplicationTranslation,
+  CustomLabelTranslation: Translations.CustomLabelTranslation,
+  CustomPageWebLinkTranslation: Translations.CustomPageWebLinkTranslation,
+  CustomTabTranslation: Translations.CustomTabTranslation,
+  FlowDefinitionTranslation: Translations.FlowDefinitionTranslation,
+  PromptTranslation: Translations.PromptTranslation,
+  GlobalQuickActionTranslation: Translations.GlobalQuickActionTranslation,
+  ReportTypeTranslation: Translations.ReportTypeTranslation,
+  ScontrolTranslation: Translations.ScontrolTranslation,
+  PromptVersionTranslation: Translations.PromptVersionTranslation,
+  FlowTranslation: Translations.FlowTranslation,
+  FlowChoiceTranslation: Translations.FlowChoiceTranslation,
+  FlowScreenTranslation: Translations.FlowScreenTranslation,
+  FlowStageTranslation: Translations.FlowStageTranslation,
+  FlowChoiceUserInputTranslation: Translations.FlowChoiceUserInputTranslation,
+  FlowInputValidationRuleTranslation:
+    Translations.FlowInputValidationRuleTranslation,
+  FlowScreenFieldTranslation: Translations.FlowScreenFieldTranslation,
+  ReportTypeSectionTranslation: Translations.ReportTypeSectionTranslation,
+  ReportTypeColumnTranslation: Translations.ReportTypeColumnTranslation,
 })
